e7064d91cdc5d46bc4279ab41cc993322bdfce87
Marco Tessitore <marcotexcode@gmail.com>
2021-05-28 14:49:09 +0200

    add: style

--- js/script.js
+++ js/script.js
@@ -26,6 +26,12 @@ if (giocatore > computer) {
 
 }
 
+
+document.getElementById('giocatore').innerHTML = 'Il giocatore ha scelto: ' + giocatore;
+document.getElementById('computer').innerHTML = 'Il computer ha scelto: ' + computer;
+document.getElementById('risultato').innerHTML = vincitore;
+
+
 console.log('Il giocatore ha scelto: ' + giocatore);
 console.log('Il computer ha scelto: ' + computer);
 console.log(vincitore);
