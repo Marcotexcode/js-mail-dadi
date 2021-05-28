cffd27aa85ff464eb73194b86ca4418964c3ebcb
Marco Tessitore <marcotexcode@gmail.com>
2021-05-28 14:13:29 +0200

    add: script gioco dati

--- js/script.js
+++ js/script.js
@@ -7,4 +7,25 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
-// 1 - generare numeri random
+// 1 - Generare numeri random
+
+var giocatore = Math.floor(Math.random() * 6) + 1;
+var computer = Math.floor(Math.random() * 6) + 1;
+
+var vincitore = 'Ha vinto il Computer';
+
+// 2 - Stabilire il vincitore
+
+if (giocatore > computer) {
+
+    vincitore = 'Ha vinto il Giocatore';
+
+} else if (giocatore == computer) {
+ 
+    vincitore = 'Pareggio';
+
+}
+
+console.log('Il giocatore ha scelto: ' + giocatore);
+console.log('Il computer ha scelto: ' + computer);
+console.log(vincitore);
