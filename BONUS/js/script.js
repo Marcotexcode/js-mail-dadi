d390700c827e34c5c149922c7b7ebb3fa3968954
Marco Tessitore <marcotexcode@gmail.com>
2021-05-28 17:03:16 +0200

    correzione

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -4,8 +4,18 @@
 // Stampare nella console tutti i numero da 10 a 30 (compresi) che sono multipli di 3.
 
 
-for(var i = 10; i < 30; i += 3){
-    console.log(i);
+// for(var i = 10; i < 30; i += 3){
+//     console.log(i);
+// }
+
+// console.log(30);
+
+
+for(var i = 10; i <= 30; i++){
+
+    if(i % 3 == 0) {
+        console.log(i);
+    }
+    
 }
 
-console.log(30);
